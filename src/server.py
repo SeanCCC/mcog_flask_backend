0d694725229dd2fe333cfc608550001e4d95b2da
seanccc <sean831004@gmail.com>
2018-10-02 11:29:17 +0800

    basic

---
+++ src/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+# Garmin Callbacks
+@app.route('/mcog/garmin_cb/dailies', methods=['POST'])
+def get_store():
+    return jsonify({'message': 'store not found'})
+
+
+app.run(port=5000, debug=True)
