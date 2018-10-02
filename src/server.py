a6e659fc7bcd825b43cc7c53b989f8ea1d8afe52
seanccc <sean831004@gmail.com>
2018-10-02 22:35:53 +0800

    https setup

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 # Garmin Callbacks
 @app.route('/mcog/garmin_cb/dailies', methods=['POST'])
 def get_store():
@@ -8,5 +9,5 @@ def get_store():
 
 
 app.run(host='0.0.0.0', port=443, debug=True, ssl_context=(
-        "../../https_keys/server-cert.pem",
-        "../../https_keys/server-key.pem"))
+        "/etc/letsencrypt/live/cmogflaskbackend.minuku.org/cert.pem",
+        "/etc/letsencrypt/live/cmogflaskbackend.minuku.org/privkey.pem"))
