7200ef3d12d489c10afb08c0910b8390e530da0a
seanccc <sean831004@gmail.com>
2018-10-02 21:32:06 +0800

    minor fix

--- src/server.py
+++ src/server.py
@@ -7,6 +7,6 @@ def get_store():
     return jsonify({'message': 'store not found'})
 
 
-app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=(
+app.run(host='0.0.0.0', port=443, debug=True, ssl_context=(
         "../../https_keys/server-cert.pem",
         "../../https_keys/server-key.pem"))
