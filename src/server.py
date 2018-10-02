fdb23bc97b9035afed54088897f5a81e0ee9a868
seanccc <sean831004@gmail.com>
2018-10-02 21:14:03 +0800

    self-assigned https

--- src/server.py
+++ src/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-
 # Garmin Callbacks
 @app.route('/mcog/garmin_cb/dailies', methods=['POST'])
 def get_store():
     return jsonify({'message': 'store not found'})
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=(
+        "../../https_keys/server-cert.pem",
+        "../../https_keys/server-key.pem"))
