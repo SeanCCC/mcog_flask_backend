7b3bb32866029adc45bd6bda7972b33c1a58b91c
seanccc <sean831004@gmail.com>
2018-10-03 10:32:42 +0800

    update

--- src/server.py
+++ src/server.py
@@ -5,58 +5,8 @@ app = Flask(__name__)
 # Garmin Callbacks
 
 
-@app.route('/mcog/garmin_cb/dailies', methods=['POST'])
-def dailies_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/activities', methods=['POST'])
-def activitis_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/manuallyupatedact', methods=['POST'])
-def manuallyupatedact_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/epochs', methods=['POST'])
-def epochs_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/sleeps', methods=['POST'])
-def sleeps_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/bodycomperessions', methods=['POST'])
-def bodycomperessions_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/thirdpartydailies', methods=['POST'])
-def thirdpartydailies_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/stress', methods=['POST'])
-def stress_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/usermetrics', methods=['POST'])
-def usermetrics_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/movelq', methods=['POST'])
-def movelq_cb():
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/mcog/garmin_cb/pulseox', methods=['POST'])
-def pulseox_cb():
+@app.route('/mcog/garmin_cb/<string:name>', methods=['POST'])
+def garmin_cb(name):
     return jsonify({'message': 'store not found'})
 
 
