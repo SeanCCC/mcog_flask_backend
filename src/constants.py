5901fe0fa1270f81a4a7ac1df5336c5443bfb6aa
I-Ju <liniju@gapp.nthu.edu.tw>
2018-10-04 08:10:37 +0000

    Update permission of Constants

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+class Constants:
+
+	## Server config
+	# PUBLIC_DNS = "ec2-52-15-224-78.us-east-2.compute.amazonaws.com"
+	PUBLIC_DNS = "https://cmogflaskbackend.minuku.org"
+
+	## HTTPS requirements
+	FULLCHAIN = "/etc/letsencrypt/live/cmogflaskbackend.minuku.org/fullchain.pem"
+	PRIVKEY = "/etc/letsencrypt/live/cmogflaskbackend.minuku.org/privkey.pem"
+
+	## Responce context
+	API_RESPONSE = None
