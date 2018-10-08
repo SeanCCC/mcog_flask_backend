b8cc80c7ce153590e096ea57322f7526edc1d64e
I-Ju <liniju@gapp.nthu.edu.tw>
2018-10-08 07:02:44 +0000

    Add MongoDB export shell script

---
+++ data/export_collections.sh
@@ -0,0 +1,11 @@
+mongoexport --db test --collection activities --out activities.json
+mongoexport --db test --collection body_compositions --out body_compositions.json
+mongoexport --db test --collection dailies --out dailies.json
+mongoexport --db test --collection daiepochs --out epochs.json
+mongoexport --db test --collection manually_updated_activities --out manually_updated_activities.json
+mongoexport --db test --collection moveIQ --out moveIQ.json
+mongoexport --db test --collection pulse_ox --out pulse_ox.json
+mongoexport --db test --collection sleeps --out sleeps.json
+mongoexport --db test --collection stress --out stress.json
+mongoexport --db test --collection third_party_dailies --out third_party_dailies.json
+mongoexport --db test --collection user_metrics --out user_metrics.json
